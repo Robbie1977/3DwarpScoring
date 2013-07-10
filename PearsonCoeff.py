b5eecf56d742fdd348d565714ba1d514c0695b81
Robbie1977 <robbie.court@gmail.com>
2013-07-10 22:49:56 +0100

    Cleaning up
    removing old commented code

--- PearsonCoeff.py
+++ PearsonCoeff.py
@@ -16,17 +16,6 @@ else:
     Nd1 = np.squeeze(np.asarray(data1,dtype=np.float128))
     Nd2 = np.squeeze(np.asarray(data2,dtype=np.float128))
                                         
-    #Ravg = np.average(Nd1)  
-    #R=np.subtract(Nd1,Ravg)
-    #
-    #Gavg = np.average(Nd2)  
-    #G=np.subtract(Nd2,Gavg)
-    #
-    #N=np.sum(np.multiply(R,G))
-    #D=np.sqrt(np.multiply(np.square(np.sum(R)),np.square(np.sum(G))))
-    #
-    #r=N/D
-    
     r=np.sum(np.multiply(np.subtract(Nd1,np.average(Nd1)),np.subtract(Nd2,np.average(Nd2))))/np.sqrt(np.multiply(np.sum(np.square(np.subtract(Nd1,np.average(Nd1)))),np.sum(np.square(np.subtract(Nd2,np.average(Nd2))))))
       
     print 'The alignment has a Pearson\'s Coefficient r value of: %0.60f (1=perfect)'% r
