84ca126e8988aae9579026f904506e3d6894733d
Robert Court <robbie.court@gmail.com>
2014-01-15 10:57:13 +0000

    added quiet mode for direct reporting of score

--- OverlapCoeff.py
+++ OverlapCoeff.py
@@ -5,15 +5,24 @@ import nrrd
 if (len(sys.argv) < 4):
     print 'Error: missing arguments!'
 else:
+    
+    quiet = False
+    if (str(sys.argv[3]).upper() == 'Q'):
+        quiet = True
 
-    print 'Checking alignment for ', str(sys.argv[1]), ' against the template (', str(sys.argv[2]), ')...'
+    if (quiet == False):
+        print 'Checking alignment for ', str(sys.argv[1]), ' against the template (', str(sys.argv[2]), ')...'
   
     data1, header1 = nrrd.read(str(sys.argv[2]))
         
     data2, header2 = nrrd.read(str(sys.argv[1])) 
       
     if (data1.size <> data2.size):
-        print '\n\nError: Images must be the same size!!'
+        if (quiet == False):
+            print '\n\nError: Images must be the same size!!'
+        else:
+            print '%0.100f'% 0
+        sys.exit(1)
     else:  
       
         Nd1 = np.squeeze(np.asarray(data1,dtype=np.float128))
@@ -21,14 +30,17 @@ else:
                                         
         r=np.sum(np.multiply(Nd1,Nd2))/np.sqrt(np.multiply(np.sum(np.square(Nd1)),np.sum(np.square(Nd2))))
         
-        print 'The alignment has a overlap coefficient value of: %0.60f (1=perfect)'% r
+        if (quiet == False):
+            print 'The alignment has a overlap coefficient value of: %0.60f (1=perfect)'% r
         
-        print 'Outputing results to ', str(sys.argv[3])
+            print 'Outputing results to ', str(sys.argv[3])
         
-        with open(str(sys.argv[3]), "a") as myfile: 
-            myfile.write('{0:.100f}'.format(r) + ', Overlap Coeffient, ' + str(sys.argv[1]) + ', ' + str(sys.argv[2])+ '\n')
+            with open(str(sys.argv[3]), "a") as myfile: 
+                myfile.write('{0:.100f}'.format(r) + ', Overlap Coeffient, ' + str(sys.argv[1]) + ', ' + str(sys.argv[2])+ '\n')
         
-        print 'Done.'
+            print 'Done.'
+        else:    
+            print '%0.100f'% r
         
   
 
