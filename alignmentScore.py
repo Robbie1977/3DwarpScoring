84f59d00ba29ca88ac1f7d31a46b48d21491ce7b
Robert Court <robbie.court@gmail.com>
2015-08-21 14:11:55 -0400

    supporting direct calls

--- alignmentScore.py
+++ alignmentScore.py
@@ -7,3 +7,11 @@ def score(template, alignment):
     # template file, alignment file (both NRRD)
     result = np.mean([np.float128(ci.rateOne(alignment ,results=None, methord=slicescore.OverlapCoeff, template=template)),np.float128(ci.rateOne(alignment ,results=None, methord=slicescore.avgOverlapCoeff, template=template))])
     return result
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 3):
+        print 'Error: missing arguments!'
+        print 'e.g. python alignmentScore.py template.nrrd alignment.nrrd'
+    else:
+        print str(score(sys.argv[1],sys.argv[2]))
+        
