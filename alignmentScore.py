3a383c031edfb39c2405aa8081d6f4b66ca8a2b0
Robert Court <robbie.court@gmail.com>
2016-02-21 02:59:57 +0000

    cleaning code

--- alignmentScore.py
+++ alignmentScore.py
@@ -1,4 +1,4 @@
-# Gives a score between 0 (bad) and 1 (excellant) for the alignment of two NRRD samples
+# Gives a score between 0 (bad) and 1 (excellent) for the alignment of two NRRD samples
 import CheckImages as ci
 import slicescore
 import numpy as np
@@ -7,17 +7,21 @@ import sys
 
 def score(template, alignment):
     # template file, alignment file (both NRRD)
-    result = np.mean([np.float128(ci.rateOne(alignment ,results=None, methord=slicescore.OverlapCoeff, template=template)),np.float128(ci.rateOne(alignment ,results=None, methord=slicescore.avgOverlapCoeff, template=template))])
+    result = np.mean(
+        [np.float128(ci.rateOne(alignment, results=None, methord=slicescore.OverlapCoeff, template=template)),
+         np.float128(ci.rateOne(alignment, results=None, methord=slicescore.avgOverlapCoeff, template=template))])
     return result
 
+
 if __name__ == "__main__":
-    if (len(sys.argv) < 3):
+    if len(sys.argv) < 3:
         print 'Error: missing arguments!'
         print 'e.g. python alignmentScore.py alignment.nrrd template.nrrd [results.csv]''
     else:
-        r = score(sys.argv[2],sys.argv[1])
+        r = score(sys.argv[2], sys.argv[1])
         print 'The final alignment score is ' + str(r)
-        if (len(sys.argv) > 3):
-        with open(str(sys.argv[3]), "a") as myfile:
-            myfile.write('{0:.100f}'.format(float(r)) + ', Alignment Score, ' + str(sys.argv[1]) + ', ' + str(sys.argv[2]) + '\n')
+        if len(sys.argv) > 3:
+            with open(str(sys.argv[3]), "a") as myfile:
+                myfile.write('{0:.100f}'.format(float(r)) + ', Alignment Score, ' + str(sys.argv[1]) + ', ' + str(
+                    sys.argv[2]) + '\n')
         print 'Done.'
--- xscore.py
+++ xscore.py
@@ -1,33 +1,39 @@
 import nrrd
 import sys, warnings
-from numpy import int,round,linspace, newaxis, shape, array, uint32, uint8, max, sqrt, abs, mean, dtype, int32, add, divide, subtract, sum
-#from matplotlib.pyplot import imshow, figure, show, colorbar
-#import matplotlib.cm as cm
+from numpy import int, round, linspace, newaxis, shape, array, uint32, uint8, max, sqrt, abs, mean, dtype, int32, add, \
+    divide, subtract, sum
+
+
+# from matplotlib.pyplot import imshow, figure, show, colorbar
+# import matplotlib.cm as cm
 
 def xslice(data):
     """Returns two stacked diagonal slices through a 3d image array."""
-    data = array(data,ndmin=3)
-    l=max(shape(data)[0:2])
-    y=array([round(i) for i in linspace(0,shape(data)[0]-1,l)],dtype=uint32)
-    x=array([round(i) for i in linspace(0,shape(data)[1]-1,l)],dtype=uint32)
-    a = data[y,x,:]
-    b = data[y[::-1],x,:]
+    data = array(data, ndmin=3)
+    l = max(shape(data)[0:2])
+    y = array([round(i) for i in linspace(0, shape(data)[0] - 1, l)], dtype=uint32)
+    x = array([round(i) for i in linspace(0, shape(data)[1] - 1, l)], dtype=uint32)
+    a = data[y, x, :]
+    b = data[y[::-1], x, :]
 
-    return array([a,b])
+    return array([a, b])
 
-def RMSdiff(data1,data2):
+
+def RMSdiff(data1, data2):
     """Returns the RMS difference between two images."""
-    return sqrt(mean(abs(data1-(data2+0.0))**2.0))
+    return sqrt(mean(abs(data1 - (data2 + 0.0)) ** 2.0))
 
-def OverlapCoeff(data1,data2):
+
+def OverlapCoeff(data1, data2):
     """Returns the Overlap Coefficent between two images."""
-    
-    return (sum(data1*data2))/(sqrt(sum(data1**2)*sum(data2**2)+0.0))
 
-def symTest(function,data):
+    return (sum(data1 * data2)) / (sqrt(sum(data1 ** 2) * sum(data2 ** 2) + 0.0))
+
+
+def symTest(function, data):
     """Applies the given function to the diagonal slices output from xslice. Can be used to assess the symetry of a 3D image using a comparison function such as OverlapCoeff."""
     if data.ndim < 3:
         warnings.warn("must be used with data output from xslice", SyntaxWarning)
         return False
     else:
-        return function(data[0],data[1])
+        return function(data[0], data[1])
